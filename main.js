f618fa4293cebfb2f8ea939469bb99891a94fec0
suchi <suchibankoti95@gmail.com>
2022-12-17 18:48:40 +0530

    add description and should display for each letter of search

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var form = document.getElementById('addForm');
 let itemList = document.getElementById("items")
+let searchBar = document.getElementById("filter")
+searchBar.addEventListener("keyup", filter)
 form.addEventListener("submit", additem)
+itemList.addEventListener("click", removeItem)
 function additem(e) {
     e.preventDefault();
     let input = document.getElementById("item")
+    let descriptionInput = document.getElementById("des")
     let newLi = document.createElement("li")
     newLi.classList.add("list-group-item")
-    newLi.appendChild(document.createTextNode(input.value))
+    newLi.appendChild(document.createTextNode("Name of the item-  " + input.value + " , " + "description- " + descriptionInput.value))
     itemList.appendChild(newLi)
 
     let deleteBtn = document.createElement("button")
@@ -17,8 +21,20 @@ function additem(e) {
     let editBtn = document.createElement("button")
     editBtn.textContent = "Edit"
     newLi.appendChild(editBtn)
-    console.log(input.value)
 }
-function removeItem() {
 
+function removeItem(e) {
+    let answer = confirm("are you sure?")
+    let li = e.target.parentElement
+    answer ? itemList.removeChild(li) : ""
+
+}
+function filter(e) {
+    let searchInput = e.target.value
+    let list = document.getElementsByTagName("li")
+    Array.from(list).forEach(item => {
+        let text = item.firstChild.textContent
+        text.indexOf(searchInput) != -1 ? item.style.display = "block" : item.style.display = "none"
+    }
+    )
 }
